adce9c45e58100be05e1d557cbc0e92d7520642f
Mirembe Peace Mercy <peace2de@gmail.com>
2024-09-06 17:53:42 +0300

    learning py

--- lesson.py
+++ lesson.py
@@ -1,7 +1,12 @@
 #print('hello world')
 
-""" a = 56
+"""a = 56
 b = 90
 sum = a + b
 print(sum)
- """
+
+
+# \n is used to break a line like in C
+
+print("The answer is",sum)# prints the statement and the answer"""
+
